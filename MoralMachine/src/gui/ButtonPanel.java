1d437efb4e029cfd2a25907673a10e8f35675374
mrskineto <neespunk@gmail.com>
2016-10-20 21:08:29 +0000

    Añadiendo cambios

--- MoralMachine/src/gui/ButtonPanel.java
+++ MoralMachine/src/gui/ButtonPanel.java
@@ -16,17 +16,27 @@ public class ButtonPanel extends JPanel {
 	
 	private JButton button_start;
 	private JButton button_exit;	
-	
+	private JButton car;
+	private JButton finish;
+	private JButton reset_characters;
+	private JButton reset_path;
 	
 public ButtonPanel() { 
 		
 		
 		button_start = new JButton("COMENZAR"); 
 		button_exit = new JButton("SALIR");
+		car = new JButton();
+		finish = new JButton();
+		reset_characters = new JButton("Reiniciar coche");
+		reset_path = new JButton("Reiniciar camino");
+		
+		
 		this.add_characteristics();
 		this.add_components();
 		this.add_Layout(); 
-		this.setVisible(true); 	
+		this.edit_components(); 
+		this.setVisible(false); 	
 	}
 
 	public void add_characteristics() { 
@@ -41,12 +51,25 @@ public ButtonPanel() {
 		
 		this.add(button_start);
 		this.add(button_exit); 
+		this.add(car);
+		this.add(finish);
+		this.add(reset_characters);
+		this.add(reset_path);
 	}
 	
 	public void add_Layout() { 
 		
 		button_start.setBounds(20,30,100,20); 
 		button_exit.setBounds(150,30,100,20); 
+		car.setBounds(300,30,20,36);
+		finish.setBounds(350,30,30,22);
+	}
+	
+public void edit_components() { 
+		
+		car.setIcon(new ImageIcon("res/images/coche-verde.png"));
+		finish.setIcon(new ImageIcon("res/images/carrera.gif"));
+		
 	}
 	
 	@Override
@@ -56,4 +79,49 @@ public ButtonPanel() {
 		this.setOpaque(false); 
 		super.paint(g); 
 	}
+	
+	public JButton getcar() {
+		return car;
+	}
+
+	public void setButton_spaceman(JButton car) {
+		this.car = car;
+	}
+
+	public JButton getfinish() {
+		return finish;
+	}
+
+	public void setButton_objetive(JButton finish) {
+		this.finish = finish;
+	}
+
+
+
+	public JButton getreset_characters() {
+		return reset_characters;
+	}
+	
+	public void setreset_characters(JButton reset_characters) {
+		this.reset_characters = reset_characters;
+	}
+
+	public JButton getreset_path() {
+		return reset_path;
+	}
+
+	public void setreset_path(JButton reset_path) {
+		this.reset_path = reset_path;
+	}
+
+	public JButton getButton_exit() {
+		return button_exit;
+	}
+
+	public void setButton_exit(JButton button_exit) {
+		this.button_exit = button_exit;
+	}
+
+	
+	
 }
--- MoralMachine/src/main/GameController.java
+++ MoralMachine/src/main/GameController.java
@@ -9,10 +9,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
+import javax.swing.*;
+
 
 public class GameController extends JFrame implements ActionListener, MouseListener 
 {
@@ -23,6 +21,8 @@ public class GameController extends JFrame implements ActionListener, MouseListe
 	private ButtonPanel botonpanel;
 	private Information information;
 	private int button_selection; 
+	private boolean car_selected; 
+	private boolean finish_selected; 
 	private boolean checkbox_path_checked;
 
 public GameController() {
@@ -32,6 +32,8 @@ public GameController() {
 		backgroundstartpanel = new Background(); 	
 		button_selection = 0; 
 		checkbox_path_checked = false; 
+		finish_selected = false;
+		car_selected = false;
 		botonpanel = new ButtonPanel();
 		information = new Information();
 		this.add_characteristics(); 
@@ -77,15 +79,46 @@ public GameController() {
 	
 public void addActionListener() { 
 	
-
+		botonpanel.getcar().addActionListener(this); 
+		botonpanel.getfinish().addActionListener(this);
+		botonpanel.getreset_characters().addActionListener(this); 
+		botonpanel.getreset_path().addActionListener(this);
+		botonpanel.getButton_exit().addActionListener(this); 
 		backgroundstartpanel.getStartpanel().getButton_start().addActionListener(this); 
 		backgroundstartpanel.getStartpanel().getButton_exit().addActionListener(this); 
 	}
 	
 	@Override
 	public void actionPerformed(ActionEvent e) { 
+		if(e.getSource() == botonpanel.getcar()) { 
+			this.button_selection = 1; 
+		}
+		if(e.getSource() == botonpanel.getfinish()) { 
+			this.button_selection = 2; 
+		}
+	
+		if(e.getSource() == botonpanel.getreset_characters()) { 
+			this.button_selection = 0; 
+			this.car_selected = false;
+			this.finish_selected = false; 
+			//matrixpanel.getGame_board().reset_characters(); 
+			matrixpanel.repaint(); 
+		}
+		if(e.getSource() == botonpanel.getreset_path()) {
+			checkbox_path_checked = false;
+			//matrixpanel.getGame_board().reset_path();
+			//matrixpanel.getGame_board().set_value(minimum_path.getOrigin_x(),minimum_path.getOrigin_y(),80);
+			//matrixpanel.getGame_board().set_value(minimum_path.getDestiny_x(),minimum_path.getDestiny_y(),100);
+			matrixpanel.repaint();
+		}
+		
+		if(e.getSource() == botonpanel.getButton_exit()) { 
+			new GameController(); 
+		}
+		
 		if(e.getSource() == backgroundstartpanel.getStartpanel().getButton_start()) {
 			backgroundstartpanel.setVisible(false); 
+			botonpanel.setVisible(true);
 			this.initialize_matrix_panel(); 
 			matrixpanel.getGame_board();
 			matrixpanel.repaint();
@@ -97,7 +130,27 @@ public void addActionListener() {
 	
 	@Override
 	public void mouseClicked(MouseEvent e) {  
+		int scale = matrixpanel.getScale_game(); 
+		int coordenada_x = e.getX(); 
+		int coordenada_y = e.getY(); 
+		int position_x = (int)(coordenada_x/scale);
+		int position_y = (int)(coordenada_y/scale);
 		
+		if((this.getButton_selection() == 1) && 
+		   (this.car_selected == false) &&
+		   (matrixpanel.getGame_board().get_value(position_y, position_x) > 3) &&
+		   (matrixpanel.getGame_board().get_value(position_y, position_x) < 80)) { 
+			matrixpanel.getGame_board().set_value(position_y,position_x,80); 
+			this.car_selected = true;
+		}
+		if((this.getButton_selection() == 2) && 
+		   (this.car_selected == false) &&
+		   (matrixpanel.getGame_board().get_value(position_y, position_x) > 3) &&
+		   (matrixpanel.getGame_board().get_value(position_y, position_x) < 80)) { 
+			matrixpanel.getGame_board().set_value(position_y,position_x,100); 
+			this.finish_selected = true; 
+		}
+		matrixpanel.repaint(); // Nueva llamada al método paint.
 	}
 	
 	
