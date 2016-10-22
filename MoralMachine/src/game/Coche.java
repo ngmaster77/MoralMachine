cbd5f8b8ef7a7877343a5e14f4d9f996a0fa1563
Samuel Ramos <alu0100815146@ull.edu.es>
2016-10-22 16:48:05 +0100

    Modificando la clase coche

--- MoralMachine/src/game/Coche.java
+++ MoralMachine/src/game/Coche.java
@@ -4,47 +4,79 @@ public class Coche
 {
 	private int xC;
 	private int yC;
+	private int nPasajeros;
 	
 	public Coche()
 	{
 		this.xC = 0;
 		this.yC = 0;
 	}
+	
 	public Coche(int x, int y)
 	{
 		this.xC = x;
 		this.yC = y;
 	}
+	
 	public int getxC()
 	{
 		return xC;
 	}
+	
 	public int getyC()
 	{
 		return yC;
 	}
+	
+	public int getnPasajeros()
+	{
+		return nPasajeros;
+	}
+	
 	public void setxC(int x)
 	{
 		this.xC = x;
 	}
+	
 	public void setyC(int y)
 	{
 		this.yC = y;
 	}
+	
+	public void setnPasajeros(int nPasajeros)
+	{
+		this.nPasajeros = nPasajeros;
+	}
+	
 	public void moveForward()
 	{
 		setxC(getxC()-1);
 	}
+	
 	public void moveBack()
 	{
 		setxC(getxC()+1);
 	}
+	
 	public void moveRight()
 	{
 		setyC(getyC()+1);
 	}
+	
 	public void moveLeft()
 	{
 		setyC(getyC()-1);
+	}
+	
+	public boolean fullPasajeros()
+	{
+		if(nPasajeros == 5)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
 	}
 }
